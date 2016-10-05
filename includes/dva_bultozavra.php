31593f6ebbe23a251a749ecdaa6b5f10ef4a38f8
Berzeg07 <berezg86@gmail.com>
2016-10-05 17:57:06 +0400

    4.10

---
+++ includes/dva_bultozavra.php
@@ -0,0 +1,17 @@
+
+<section class="ab-bultozavr">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12 col-sm-12 col-xs-12">
+				<div class="ab-txt-top">
+					<span>
+						«Два бульбазавра, раттата и пикачу. В принципе, достойно»<br>
+						Артем Никуличев подводит итоги турнира в Хорватии
+					</span>
+					<span class="ab-txt-bottom">18.08.2016 | Антон Виноградов</span>
+				</div>
+				
+			</div>
+		</div>
+	</div>
+</section>
---
+++ includes/video.php
@@ -0,0 +1,20 @@
+
+<section class="ab-video-wrap">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-8 col-sm-12 col-xs-12">
+				<div class="videowrapper">
+					<iframe width="515" height="" src="https://www.youtube.com/embed/WnKYRTrbUiI?rel=0" frameborder="0" allowfullscreen></iframe>
+				</div>
+			</div>	
+			<div class="col-md-4 col-sm-12 col-xs-12">
+				<div class="videowrapper ab-padding">
+					<iframe width="100%" height="315" src="https://www.youtube.com/embed/WnKYRTrbUiI?rel=0" frameborder="0" allowfullscreen></iframe>
+				</div>
+				<div class="videowrapper ab-padding-2">
+					<iframe width="100%" height="315" src="https://www.youtube.com/embed/CobHfkWRkM0?rel=0" frameborder="0" allowfullscreen></iframe>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
