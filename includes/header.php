a3069e77f6a14a267d25b8866eaeebb2abd59404
Konstantin Semenov <kvsemenoff@mail.ru>
2016-10-05 17:49:23 +0400

    d

--- includes/header.php
+++ includes/header.php
@@ -2,41 +2,43 @@
 	<div class="container">
 		<div class="row">
 			<div class="col-md-3 col-sm-12 col-xs-12">
-				<a href="/">
-					<img src="img/logo.png" alt="">
-				</a>
+				<div class="logo-box">
+					<a href="/">
+						<img src="img/logo.png" class="logo" alt="">
+					</a>
+				</div>
 			</div>
 			<div class="col-md-3 col-sm-12 col-xs-12">
-
-				<div class="soc-block">
-					<div class="soc-block_item">
-						<span class="ho-header-title">Войдите</span>
-					</div>
-					<div class="soc-block_item">
-					
-						<div class="soc">
-							<a href="#" class="vk"><i class="fa fa-vk" aria-hidden="true"></i></a>
-							<a href="#" class="fb"><i class="fa fa-facebook" aria-hidden="true"></i></a>
-							<a href="#" class="tw"><i class="fa fa-twitter" aria-hidden="true"></i></a>
+				<div class="soc-block-search">
+					<div class="soc-block-1">
+						<div class="soc-block_item">
+							<span class="ho-header-title">Войдите</span>
+						</div>
+						<div class="soc-block_item">
+						
+							<div class="soc">
+								<a href="#" class="vk"><i class="fa fa-vk" aria-hidden="true"></i></a>
+								<a href="#" class="fb"><i class="fa fa-facebook" aria-hidden="true"></i></a>
+								<a href="#" class="tw"><i class="fa fa-twitter" aria-hidden="true"></i></a>
+							</div>
+						</div>
+						<div class="soc-block_item">
+							<a href="#">Напишите в блог</a>
 						</div>
 					</div>
-					<div class="soc-block_item">
-						<a href="#">Напишите в блог</a>
-					</div>
-				</div>
-				<div class="soc-block">
+					<div class="soc-block-2">
 
-					<div class="search-box">
-						<form action="#">
-							<input type="text" placeholder="Поиск">
-							<input type="submit" value="">
-						</form>
+						<div class="search-box">
+							<form action="#">
+								<input type="text" placeholder="Поиск">
+								<input type="submit" value="">
+							</form>
+						</div>
 					</div>
 				</div>
-				
 			</div>
 			<div class="col-md-3 col-sm-12 col-xs-12">
-				<div class="soc-block sc-bg">
+				<div class="sc-bg">
 
 					<div class="singup">
 						<span class="ho-header-title"><strong>Присоединяйтесь:</strong></span>
@@ -52,8 +54,46 @@
 				
 			</div>
 			<div class="col-md-3 col-sm-12 col-xs-12">
-				<span class="ho-header-title"><strong>Качайте наше приложение:</strong></span>
+				<div class="download">
+					<span class="ho-header-title"><strong>Качайте наше приложение:</strong></span>
+					<div class="store-box">
+						
+							<a href="#"><img src="img/google_play.png" alt=""></a>
+						
+							<a href="#"><img src="img/appstore.png" alt=""></a>
+						
+					</div>
+				</div>
 			</div>
 		</div>
 	</div>
+	<div class="anz-menu">
+		<input type="checkbox" id="check_1" class="anz-mobile-992"/>
+		<label class="anz-menu-ch anz-mobile-992" for="check_1"><i class="fa fa-bars" aria-hidden="true"></i><i class="fa fa-times" aria-hidden="true"></i></label>
+		<div class="anz-perspective">
+			<div class="menu-list-pressed">
+			    <ul class="main-menu">
+			    	<li><a href="#">УЧАСТНИКИ</a></li>
+    	 			<li><a href="#">РЕЗУЛЬТАТЫ</a></li>
+    	 			<li><a href="#">ДИВИЗИОНЫ</a></li>
+    	 			<li><a href="#">СТАТИСТИКА</a></li>
+    	 			<li><a href="#">МЕДИА</a></li> 			
+    	 			<li><a href="#">ПРОЕКТЫ</a></li> 			
+    	 			<li><a href="#">ОБ АСБ</a></li> 		
+			    </ul>
+		    </div>
+	   </div>
+	</div>
+</section>
+
+<section class="ho-main-menu hidden-sm hidden-xs">
+	<ul class="main-menu">
+    	<li><a href="#">УЧАСТНИКИ</a></li>
+		<li><a href="#">РЕЗУЛЬТАТЫ</a></li>
+		<li><a href="#">ДИВИЗИОНЫ</a></li>
+		<li><a href="#">СТАТИСТИКА</a></li>
+		<li><a href="#">МЕДИА</a></li> 			
+		<li><a href="#">ПРОЕКТЫ</a></li> 			
+		<li><a href="#">ОБ АСБ</a></li> 		
+    </ul>
 </section>
