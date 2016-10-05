2224b261476918161702fe46889b4d7c9c162bfa
anzarsh <anzarsh@mail.ru>
2016-10-05 17:29:40 +0300

    начало дивизионы

--- includes/divisiony.php
+++ includes/divisiony.php
@@ -1,17 +1,33 @@
-<section class="results">
+<section class="devision">
 	<div class="container">
 		<div class="row">
 			<div class="col-md-8">
-				<h2>Дивизионы</h2>
-			</div>
-			<div class="col-md-4">
-				<div id="az-select2" class="az-select">
+				<h2 class="az-left">Дивизионы</h2>
+				<div id="az-select2" class="az-select az-right az-select_style">
 					<select name="" id="">
 						<option value="1">Санкт-Петербург</option>
 						<option value="2">Москва</option>
 						<option value="3">Красноярск</option>
 					</select>
 				</div>
+				<div class="clearfix"></div>
+				<div class="tabs tabs__margin2 tabs_style">
+					<input id="tab_1" type="radio" name="tab" checked="checked"/>
+					<input id="tab_2" type="radio" name="tab"/>
+					<label for="tab_1" id="tab_l1">все</label>
+					<label for="tab_2" id="tab_l2">кирпич</label>
+					<div class="tabs_cont tabs_cont_style">
+						<div id="tab_c1">
+							<span class="tab-span">Команды играют в два круга, затем проводится Финал четырех</span>
+						</div>
+						<div id="tab_c2">
+						2
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="col-md-4">
+
 			</div>
 		</div>
 	</div>
