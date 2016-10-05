f47e2375168202c377ebabe8f5eafacaf440b3d1
Konstantin Semenov <kvsemenoff@mail.ru>
2016-10-05 13:03:39 +0400

    first commit

---
+++ includes/header.php
@@ -0,0 +1,59 @@
+<section class="ho-header-top">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-3 col-sm-12 col-xs-12">
+				<a href="/">
+					<img src="img/logo.png" alt="">
+				</a>
+			</div>
+			<div class="col-md-3 col-sm-12 col-xs-12">
+
+				<div class="soc-block">
+					<div class="soc-block_item">
+						<span class="ho-header-title">Войдите</span>
+					</div>
+					<div class="soc-block_item">
+					
+						<div class="soc">
+							<a href="#" class="vk"><i class="fa fa-vk" aria-hidden="true"></i></a>
+							<a href="#" class="fb"><i class="fa fa-facebook" aria-hidden="true"></i></a>
+							<a href="#" class="tw"><i class="fa fa-twitter" aria-hidden="true"></i></a>
+						</div>
+					</div>
+					<div class="soc-block_item">
+						<a href="#">Напишите в блог</a>
+					</div>
+				</div>
+				<div class="soc-block">
+
+					<div class="search-box">
+						<form action="#">
+							<input type="text" placeholder="Поиск">
+							<input type="submit" value="">
+						</form>
+					</div>
+				</div>
+				
+			</div>
+			<div class="col-md-3 col-sm-12 col-xs-12">
+				<div class="soc-block sc-bg">
+
+					<div class="singup">
+						<span class="ho-header-title"><strong>Присоединяйтесь:</strong></span>
+					</div>
+					<div class="soc-big">
+						<a href="#" class="vk"><i class="fa fa-vk" aria-hidden="true"></i></a>
+						<a href="#" class="youtube"><i class="fa fa-youtube" aria-hidden="true"></i></a>
+						<a href="#" class="youtube"><i class="fa fa-instagram" aria-hidden="true"></i></a>
+						<a href="#" class="fb"><i class="fa fa-facebook" aria-hidden="true"></i></a>
+						<a href="#" class="tw"><i class="fa fa-twitter" aria-hidden="true"></i></a>
+					</div>
+				</div>
+				
+			</div>
+			<div class="col-md-3 col-sm-12 col-xs-12">
+				<span class="ho-header-title"><strong>Качайте наше приложение:</strong></span>
+			</div>
+		</div>
+	</div>
+</section>
