57343537a09be9eb80c1625b8afc0aa8ae1be600
Berzeg07 <berezg86@gmail.com>
2016-10-06 17:49:10 +0400

    6.10

--- includes/foto.php
+++ includes/foto.php
@@ -8,7 +8,7 @@
 					<div class="ab-foto-txt">
 						<span>В Ростове-на-Дону прошел тренировочный лагерь<br> АСМ-ДГТУ</span>
 						<span>Брюс Боуэн съездил в тренировочный лагерь АСБ и вспомнил молодость</span>
-						<span>18.08.2016 | Антон Виноградов</span>
+						<span>18.08 | <a href="#">Антон Виноградов</a></span>
 					</div>
 				</div>
 			</div>
@@ -17,14 +17,14 @@
 					<img class="img-responsive" src="img/ab-nigga.png" alt="">
 					<div class="ab-foto-txt">
 						<span>В Иваново пройдет региональный<br> турнир АСБ 3х3</span>
-						<span>18.08.2016 | Антон Виноградов</span>
+						<span>18.08 | <a href="#">Антон Виноградов</a></span>
 					</div>
 				</div>
 				<div class="ab-small-foto-2">
 					<img class="img-responsive" src="img/ab-white.png" alt="">
 					<div class="ab-foto-txt">
 						<span>Сколько сербов может быть в <br>команде крик души</span>
-						<span>18.08.2016 | Антон Виноградов</span>
+						<span>18.08 | <a href="#">Антон Виноградов</a></span>
 					</div>
 				</div>
 			</div>
