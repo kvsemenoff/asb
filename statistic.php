941bcb38edb21749854d0eaede0d3074497853b1
engine77 <zaur.ka.00@mail.ru>
2016-10-10 13:46:03 +0300

    а

--- statistic.php
+++ statistic.php
@@ -7,6 +7,7 @@
 
 	<link rel="stylesheet" href="libs/bootstrap/bootstrap-grid-3.3.1.min.css" />
 	<link rel="stylesheet" href="libs/font-awesome-4.2.0/css/font-awesome.min.css" />
+	<link href="//netdna.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.css" rel="stylesheet">
 	<link rel="stylesheet" href="libs/fancybox/jquery.fancybox.css" />
 	<link rel="stylesheet" href="libs/owl.carousel/assets/owl.carousel.css" />
 	<link rel="stylesheet" href="libs/wow/animate.css">
@@ -43,7 +44,7 @@
 						
 				</div>
 
-				<div class="col-md-8">
+				<div class="col-md-12">
 					
 						<ul class="male">
 								<li><a href="#">МУЖЧИНЫ</a></li>
