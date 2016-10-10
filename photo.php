a3f1661fd444a6fc5fbe1104bbd58df94e5fb6fa
Berzeg07 <berezg86@gmail.com>
2016-10-10 13:39:05 +0400

    8.10

--- photo.php
+++ photo.php
@@ -35,52 +35,429 @@
 
 	<?php require_once('includes/header.php'); ?>
 	<div class="clearfix"></div>
-	<section class="main-content">
-		<div class="container">
-			<div class="row">
-				<div class="col-md-12">
-					<h1>ФОТОАЛЬБОМЫ</h1>
-					
-
-				</div>
-				<div class="col-md-8">
-
-					<div id="az-select" class="az-select az-margin-bottom10">
-						<select name="" id="" > 
-							<option value="1">Лига Белова</option>
-							<option value="2">Лига Петрова</option>
-							<option value="3">Суперфинал</option>
-							<option value="4">Дивизионы</option>
-							<option value="5">АСБ ФЕСТ</option>
-							<option value="6">Матчи звезд</option>
-							<option value="7">3x3</option>
-							<option value="8">Сборная</option>
-							<option value="9">Тренировачный лагерь</option>
-							<option value="10">Разное</option>
-						</select>
-					</div>
-					<div id="az-select1" class="az-select az-margin-bottom10">
-						<select name="" id=""> 
-							<option value="1">Сезон 2015/2016</option>
-							<option value="2">Сезон 2014/2015</option>
-							<option value="3">Сезон 2013/2014</option>
-							<option value="4">Сезон 2012/2013</option>
-							<option value="5">Сезон 2011/2012</option>
-							<option value="6">Сезон 2010/2011</option>
-							<option value="7">Сезон 2009/2010</option>
-							<option value="8">Сезон 2008/2009</option>
-							<option value="9">Сезон 2007/2008</option>							
-						</select>
-					</div>
-					<div id="az-select2" class="az-select az-margin-bottom10">
-						<select name="" id=""> 
-							<option value="1">Добавить</option>
+	
+<section class="ab-photo-wrapper">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12">
+				<h2>Фотоальбомы</h2>
+			</div>
+			<div class="clearfix"></div>
+			<div class="col-md-3">
+				<div id="az-select" class="az-select az-margin-bottom10">
+					<select name="" id="">
+						<option value="1">Все</option>
+						<option value="2">Лига Петрова</option>
+						<option value="3">Супер финал</option>
+						<option value="4">Дивизионы</option>
+						<option value="5">АСБ ФЕСТ</option>
+						<option value="6">Матчи звезд</option>
+						<option value="7">3x3</option>
+						<option value="8">Сборная</option>
+						<option value="9">Тренировочный лагерь</option>
+						<option value="9">Разное</option>
 					</select>
+				</div>
+			</div>
+			<div class="col-md-3">
+				<div id="az-select" class="az-select az-margin-bottom10">
+					<select name="" id="">
+						<option value="1">Сезон 2015/2016</option>
+						<option value="2">Сезон 2014/2015</option>
+						<option value="1">Сезон 2013/2014</option>
+						<option value="2">Сезон 2012/2013</option>
+						<option value="1">Сезон 2011/2012</option>
+						<option value="2">Сезон 2010/2011</option>
+						<option value="1">Сезон 2009/2010</option>
+						<option value="2">Сезон 2008/2009</option>
+						<option value="1">Сезон 2007/2008</option>
+					</select>
+				</div>
+			</div>
+			<div class="col-md-3">
+				<div id="az-select" class="az-select az-margin-bottom10">
+					<select name="" id="">
+						<option value="1">Добавить в альбом</option>
+					</select>
+				</div>
+			</div>
+			<div class="col-md-3">
+				
+			</div>
+			<div class="clearfix"></div>
+		</div>
+	</div>
+</section>
+<section class="ab-big-andsmall">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-8 col-sm-12 col-xs-12">
+				<div class="videowrapper ab-margin-bottomm">
+					<img class="img-responsive" src="img/big-photo.jpg" alt="">
+					<div class="ab-ph-main">
+						<span><a href="#"> АСБ ФЕСТ<br> (день 1)<br></a>
+						</span>
+					</div>
+					<div class="ab-ph-mini">
+						<span><a href="#">123 фото</a></span>
+					</div>
+				</div>
+			</div>	
+			<div class="col-md-4 col-sm-12 col-xs-12 ab-mma">
+				<div class="videowrapper ab-padding">
+					<img class="img-responsive" src="img/small-1.jpg" alt="">
+					<div class="ab-ph-main ab-ph-main-2">
+						<span class="ab-mma"><a href="#"> #АСБФЕСТ<br></a>
+						</span>
+					</div>
+					<div class="ab-ph-mini ab-ph-mini-2">
+						<span class="ab-mma-2"><a href="#">17 фото</a></span>
+					</div>
+				</div>
+				<div class="videowrapper ab-new-padding">
+					<img class="img-responsive" src="img/small-2.jpg" alt="">
+					<div class="ab-ph-main ab-ph-main-2">
+						<span class="ab-mma"><a href="#"> АСБ ФЕСТ <br>(день 2)<br></a>
+						</span>
+					</div>
+					<div class="ab-ph-mini ab-ph-mini-2">
+						<span class="ab-mma-2"><a href="#">191 фото</a></span>
 					</div>
 				</div>
 			</div>
+			<div class="clearfix"></div>
 		</div>
-	</section>
+	</div>
+</section>
+<section class="ab-all-photo">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-3 col-sm-12 col-xs-12">
+				<div class="ab-box-img">
+					<img class="img-responsive" src="img/ab-1.jpg" alt="">
+					<div class="ab-ph-main">
+						<span><a href="#">Сборная АСБ -<br>
+						Университетская<br>
+						сборная кореи-2</a>
+						</span>
+					</div>
+					<div class="ab-ph-mini">
+						<span><a href="#">41 фото</a></span>
+					</div>
+				</div>
+			</div>
+			<div class="col-md-3 col-sm-12 col-xs-12">
+				<div class="ab-box-img">
+					<img class="img-responsive" src="img/ab-2.jpg" alt="">
+					<div class="ab-ph-main">
+						<span><a href="#">Сборная АСБ -<br>
+						Посещение дворца<br>
+						Кенбоккун</a></span>
+						</span>
+					</div>
+					<div class="ab-ph-mini">
+						<span><a href="#">26 фото</a></span>
+					</div>
+				</div>
+			</div>
+			<div class="col-md-3 col-sm-12 col-xs-12">
+				<div class="ab-box-img">
+					<img class="img-responsive" src="img/ab-3.jpg" alt="">
+					<div class="ab-ph-main">
+						<span><a href="#">Сборная АСБ -<br>
+						Университетская<br>
+						Сборная Японии</a>
+						</span>
+					</div>
+					<div class="ab-ph-mini">
+						<span><a href="#">51 фото</a></span>
+					</div>
+				</div>
+			</div>
+			<div class="col-md-3 col-sm-12 col-xs-12">
+				<div class="ab-box-img">
+					<img class="img-responsive" src="img/ab-4.jpg" alt="">
+					<div class="ab-ph-main">
+						<span>
+						<a href="#">Фотосессия<br>
+						сборной АСБ</a>
+						</span>
+					</div>
+					<div class="ab-ph-mini">
+						<span><a href="#">50 фото</a></span>
+					</div>
+				</div>
+			</div>
+			<div class="clearfix"></div>	
+		</div>
+	</div>
+	<div class="container">
+		<div class="row">
+			<div class="col-md-3 col-sm-12 col-xs-12">
+				<div class="ab-box-img">
+					<img class="img-responsive" src="img/ab-3.jpg" alt="">
+					<div class="ab-ph-main">
+						<span><a href="#">Сборная АСБ -<br>
+						Университетская<br>
+						Сборная Японии</a>
+						</span>
+					</div>
+					<div class="ab-ph-mini">
+						<span><a href="#">51 фото</a></span>
+					</div>
+				</div>
+			</div>
+			<div class="col-md-3 col-sm-12 col-xs-12">
+				<div class="ab-box-img">
+					<img class="img-responsive" src="img/ab-4.jpg" alt="">
+					<div class="ab-ph-main">
+						<span><a href="#">Фотосессия<br>
+						сборной АСБ</a>
+						</span>
+					</div>
+					<div class="ab-ph-mini">
+						<span><a href="#">50 фото</a></span>
+					</div>
+				</div>
+			</div>
+			<div class="col-md-3 col-sm-12 col-xs-12">
+				<div class="ab-box-img">
+					<img class="img-responsive" src="img/ab-1.jpg" alt="">
+					<div class="ab-ph-main">
+						<span><a href="#">Сборная АСБ -<br>
+						Университетская<br>
+						сборная Кореи-2</a>
+						</span>
+					</div>
+					<div class="ab-ph-mini">
+						<span><a href="#">Сборная АСБ -</a><br>
+						<span><a href="#">41 фото</a></span>
+					</div>
+				</div>
+			</div>
+			<div class="col-md-3 col-sm-12 col-xs-12">
+				<div class="ab-box-img">
+					<img class="img-responsive" src="img/ab-2.jpg" alt="">
+					<div class="ab-ph-main">
+						<span><a href="#">Сборная АСБ -<br>
+						Посещение дворца<br>
+						Кенбоккун</a>
+						</span>
+					</div>
+					<div class="ab-ph-mini">
+						<span><a href="#">26 фото</a></span>
+					</div>
+				</div>
+			</div>
+			<div class="clearfix"></div>	
+		</div>
+	</div>
+	<div class="container">
+		<div class="row">
+			<div class="col-md-3 col-sm-12 col-xs-12">
+				<div class="ab-box-img">
+					<img class="img-responsive" src="img/ab-1.jpg" alt="">
+					<div class="ab-ph-main">
+						<span><a href="#">Сборная АСБ -<br>
+						Университетская<br>
+						сборная кореи-2</a>
+						</span>
+					</div>
+					<div class="ab-ph-mini">
+						<span><a href="#">41 фото</a></span>
+					</div>
+				</div>
+			</div>
+			<div class="col-md-3 col-sm-12 col-xs-12">
+				<div class="ab-box-img">
+					<img class="img-responsive" src="img/ab-2.jpg" alt="">
+					<div class="ab-ph-main">
+						<span><a href="#">Сборная АСБ -<br>
+						Посещение дворца<br>
+						Кенбоккун</a>
+						</span>
+					</div>
+					<div class="ab-ph-mini">
+						<span><a href="#">26 фото</a></span>
+					</div>
+				</div>
+			</div>
+			<div class="col-md-3 col-sm-12 col-xs-12">
+				<div class="ab-box-img">
+					<img class="img-responsive" src="img/ab-3.jpg" alt="">
+					<div class="ab-ph-main">
+						<span><a href="#">Сборная АСБ -<br>
+						Университетская<br>
+						Сборная Японии</a>
+						</span>
+					</div>
+					<div class="ab-ph-mini">
+						<span><a href="#">51 фото</a></span>
+					</div>
+				</div>
+			</div>
+			<div class="col-md-3 col-sm-12 col-xs-12">
+				<div class="ab-box-img">
+					<img class="img-responsive" src="img/ab-4.jpg" alt="">
+					<div class="ab-ph-main">
+						<span><a href="#">
+						Фотосессия<br>
+						сборной АСБ</a>
+						</span>
+					</div>
+					<div class="ab-ph-mini">
+						<span><a href="#">50 фото</a></span>
+					</div>
+				</div>
+			</div>
+			<div class="clearfix"></div>	
+		</div>
+	</div>
+	<div class="container">
+		<div class="row">
+			<div class="col-md-3 col-sm-12 col-xs-12">
+				<div class="ab-box-img">
+					<img class="img-responsive" src="img/ab-3.jpg" alt="">
+					<div class="ab-ph-main">
+						<span><a href="#">Сборная АСБ -<br>
+						Университетская<br>
+						Сборная Японии</a>
+						</span>
+					</div>
+					<div class="ab-ph-mini">
+						<span><a href="#">51 фото</a></span>
+					</div>
+				</div>
+			</div>
+			<div class="col-md-3 col-sm-12 col-xs-12">
+				<div class="ab-box-img">
+					<img class="img-responsive" src="img/ab-4.jpg" alt="">
+					<div class="ab-ph-main">
+						<span><a href="#">Фотосессия<br>
+						сборной АСБ</a>
+						</span>
+					</div>
+					<div class="ab-ph-mini">
+						<span><a href="#">50 фото</a></span>
+					</div>
+				</div>
+			</div>
+			<div class="col-md-3 col-sm-12 col-xs-12">
+				<div class="ab-box-img">
+					<img class="img-responsive" src="img/ab-1.jpg" alt="">
+					<div class="ab-ph-main">
+						<span><a href="#">Сборная АСБ -<br>
+						Университетская<br>
+						сборная Кореи-2</a>
+						</span>
+					</div>
+					<div class="ab-ph-mini">
+						<span><a href="#">41 фото</a></span>
+					</div>
+				</div>
+			</div>
+			<div class="col-md-3 col-sm-12 col-xs-12">
+				<div class="ab-box-img">
+					<img class="img-responsive" src="img/ab-2.jpg" alt="">
+					<div class="ab-ph-main">
+						<span><a href="#">Сборная АСБ -<br>
+						Посещение дворца<br>
+						Кенбоккун</a>
+						</span>
+					</div>
+					<div class="ab-ph-mini">
+						<span><a href="#">26 фото</a></span>
+					</div>
+				</div>
+			</div>
+			<div class="clearfix"></div>	
+		</div>
+	</div>
+	<div class="container">
+		<div class="row">
+			<div class="col-md-3 col-sm-12 col-xs-12">
+				<div class="ab-box-img">
+					<img class="img-responsive" src="img/ab-1.jpg" alt="">
+					<div class="ab-ph-main">
+						<span><a href="#">Сборная АСБ -<br>
+						Университетская<br>
+						сборная кореи-2</a>
+						</span>
+					</div>
+					<div class="ab-ph-mini">
+						<span><a href="#">41 фото</a></span>
+					</div>
+				</div>
+			</div>
+			<div class="col-md-3 col-sm-12 col-xs-12">
+				<div class="ab-box-img">
+					<img class="img-responsive" src="img/ab-2.jpg" alt="">
+					<div class="ab-ph-main">
+						<span><a href="#">Сборная АСБ -<br>
+						Посещение дворца<br>
+						Кенбоккун</a>
+						</span>
+					</div>
+					<div class="ab-ph-mini">
+						<span><a href="#">26 фото</a></span>
+					</div>
+				</div>
+			</div>
+			<div class="col-md-3 col-sm-12 col-xs-12">
+				<div class="ab-box-img">
+					<img class="img-responsive" src="img/ab-3.jpg" alt="">
+					<div class="ab-ph-main">
+						<span><a href="#">Сборная АСБ -<br>
+						Университетская<br>
+						Сборная Японии</a>
+						</span>
+					</div>
+					<div class="ab-ph-mini">
+						<span><a href="#">51 фото</a></span>
+					</div>
+				</div>
+			</div>
+			<div class="col-md-3 col-sm-12 col-xs-12">
+				<div class="ab-box-img">
+					<img class="img-responsive" src="img/ab-4.jpg" alt="">
+					<div class="ab-ph-main">
+						<span>
+						<a href="#">Фотосессия<br>
+						сборной АСБ</a>
+						</span>
+					</div>
+					<div class="ab-ph-mini">
+						<span><a href="#">50 фото</a></span>
+					</div>
+				</div>
+			</div>
+			<div class="clearfix"></div>	
+		</div>
+	</div>
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12 col-sm-12 col-xs-12">
+				<div class="ab-photo-but-wrap">
+					<a class="button-photo" href="#">Показать еще</a>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 	<?php require_once('includes/footer.php'); ?>
 <script>
 $(document).ready(function(){
