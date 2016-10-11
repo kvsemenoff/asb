f25280bdc6050310d38a0579ec391563214060f0
Berzeg07 <berezg86@gmail.com>
2016-10-11 11:29:52 +0400

    10.10

---
+++ includes/foto-1.php
@@ -0,0 +1,18 @@
+
+<section class="ab-foto-wrap">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12 col-sm-12 col-xs-12">
+				<div class="ab-big-foto ab-index-1">
+					<img class="img-responsive" src="img/ab-ball.png" alt="">
+					<div class="ab-foto-txt">
+						<span>В Ростове-на-Дону прошел тренировочный лагерь<br> АСМ-ДГТУ</span><br>
+						<span>Брюс Боуэн съездил в тренировочный лагерь АСБ и вспомнил молодость</span>
+						<span>18.08 | <a href="#">Антон Виноградов</a></span>
+					</div>
+				</div>
+			</div>
+			
+		</div>
+	</div>
+</section>
